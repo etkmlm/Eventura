4b306ad3a0e9c5c1bfde2e56d6b2301ddeb87325
etkmlm <furkanmahmutyilmaz@gmail.com>
2022-05-22 17:41:27 +0300

    update with coreliumsocketapi

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using CoreliumCryptionEngine;
+using CoreliumSocketAPI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,10 +25,16 @@ namespace Eventura
     public partial class MainWindow : Window
     {
         public static string IP;
-        public static Communicator communicator;
+        public static ISocketService service;
+        private const int BUFFER_SIZE = 4096;
+        private List<CoreliumSocket> allowed;
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            allowed = new List<CoreliumSocket>();
+
             if (!NetworkInterface.GetIsNetworkAvailable())
             {
                 MessageBox.Show("To use this app, you need an internet connection!");
@@ -36,75 +44,107 @@ namespace Eventura
             MouseLeftButtonDown += (a, b) => DragMove();
             btnClose.Click += (a, b) =>
             {
-                communicator?.Close();
+                service?.Stop();
                 Environment.Exit(0);
             };
             IP = new System.Net.WebClient().DownloadString("http://icanhazip.com").Replace("\\r\\n", "").Replace("\\n", "").Trim();
 
             btnConnect.Click += (a, b) =>
             {
-                communicator = new Communicator(txtIP.Text, false, txtPass.Text);
-                communicator.OnReceived += OnReceived;
+                string[] info = txtIP.Text.Split(':');
+                if (!int.TryParse(info[1], out int port))
+                {
+                    MessageBox.Show("Please enter a valid ip!");
+                    return;
+                }
+                service?.Stop();
+
+                service = new TCPClient(IP, BUFFER_SIZE, info[0], port).Start();
+                ((TCPClient)service).Connected += () =>
+                {
+                    Add("System", "Connected!");
+                    Thread.Sleep(100);
+                    Dispatcher.Invoke(() => service.Send("pass" + txtPass.Text));
+                };
+                var server = service.GetServer().SetTimeout(10);
+                allowed.Add(server);
+
+                server.Received += OnClientReceived;
+                server.Disconnected += () => Add("System", "Connection lost.");
             };
 
-            btnSend.Click += (a, b) => Send(txtMessage.Text);
+            btnSend.Click += (a, b) => Send("msg", txtMessage.Text);
             txtMessage.KeyDown += (a, b) =>
             {
                 if (b.Key == Key.Enter)
-                    Send(txtMessage.Text);
+                    Send("msg", txtMessage.Text);
             };
 
             btnStart.Click += (a, b) =>
             {
-                communicator = new Communicator(txtIP.Text, true, txtPass.Text);
-                communicator.OnReceived += OnReceived;
+                string[] info = txtIP.Text.Split(':');
+                if (!int.TryParse(info[1], out int port))
+                {
+                    MessageBox.Show("Please enter a valid ip!");
+                    return;
+                }
+                service?.Stop();
+
+                service = new TCPServer(BUFFER_SIZE, info[0], port).SetTimeout(10).Start();
+                var server = (TCPServer)service;
+                Add("System", "Server started!");
+
+                server.NameReceived += (socket, name) => Add("System", name + " joined, waiting for password...");
+                server.Received += OnServerReceived;
             };
         }
 
-        private void OnReceived(string message, System.Net.Sockets.Socket s)
+        private void OnClientReceived(byte[] buffer, int received)
         {
-            if (message.StartsWith("|SYS|"))
+            string message = Encoding.UTF8.GetString(buffer).Substring(0, received);
+
+            if (message == "kicked")
+                Add("Server", "Wrong password!");
+            else if (message.StartsWith("msg"))
             {
-                string m = message.Substring(5);
-                if (m.StartsWith("pass") && communicator.mode)
+                string[] data = message.Substring(3).Split(new char[] { ',' }, 2);
+                if (data.Length == 1)
+                    Add("System", data[0]);
+                else
+                    Add(data[0], data[1]);
+            }
+        }
+
+        private void OnServerReceived(CoreliumSocket socket, byte[] buffer) => Dispatcher.Invoke(() =>
+        {
+            string message = Encoding.UTF8.GetString(buffer);
+
+            if (message.StartsWith("pass"))
+            {
+                if (message.Substring(4) == txtPass.Text)
                 {
-                    if (communicator.pass != m.Substring(4))
-                    {
-                        communicator.SendTo("You entered a wrong password!", s, "System");
-                        communicator.sockets.Remove(s);
-                        communicator.allowed.Remove(s);
-                        communicator.Send(s.RemoteEndPoint.ToString() + " kicked: Wrong pass.", "System");
-                    }
-                    else
-                    {
-                        communicator.allowed.Add(s);
-                        communicator.Send(s.RemoteEndPoint.ToString() + " joined the chat!", "System");
-                    }
+                    allowed.Add(socket);
+                    Send("msg", socket.Name + " joined to chat!", from: "System");
+                }
+                else
+                {
+                    socket.Send("kicked");
+                    Send("msg", socket.Name + " kicked due to entered wrong password!", from: "System");
+                    socket.Close();
                 }
             }
-            else
-            {
-                string[] content = message.Split(new string[] { ":/:" }, StringSplitOptions.None);
-                if (content[0] != "System" && !communicator.allowed.Contains(s) && communicator.mode)
-                    return;
-                if (content.Length == 1)
-                    Add("", content[0]);
-                else
-                    Add(content[0], content[1]);
-            }
-        }
+            else if (message.StartsWith("msg"))
+                Send("msg", message.Substring(3), allowed.Where(x => x != socket), socket.Name);
+        });
 
-        private void Send(string text)
+        private void Send(string prefix, string msg, IEnumerable<CoreliumSocket> to = null, string from = null) => Dispatcher.Invoke(() =>
         {
-            if (string.IsNullOrWhiteSpace(text))
-                return;
-            communicator?.Send(text);
-            Add(IP, text);
+            (to?.ToList() ?? allowed).ForEach(x => x.Send(prefix + (service is TCPServer ? IP + "," : "") + msg));
+            Add(from ?? IP, msg.Replace(from == null ? IP + "," : from + ",", ""));
             txtMessage.Clear();
-        }
+        });
 
-        private void Add(string user, string msg)
-        {
+        private void Add(string user, string msg) =>
             Dispatcher.Invoke(() =>
             {
                 Run run = new Run(msg);
@@ -115,6 +155,5 @@ namespace Eventura
                 txtMessages.Document.Blocks.Add(p);
                 txtMessages.ScrollToEnd();
             });
-        }
     }
 }
